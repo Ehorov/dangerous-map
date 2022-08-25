3dd18cc516bddc22a0a54c89a846c4994b1e00c6
Ehorov <82938317+Ehorov@users.noreply.github.com>
2022-08-25 11:31:23 +0300

    current marker add

--- src/components/CurrentLocationMarker/CurrentLocationMarker.jsx
+++ src/components/CurrentLocationMarker/CurrentLocationMarker.jsx
@@ -1,11 +1,5 @@
 import { Marker } from "@react-google-maps/api";
 
 export const CurrentLocationMarker = ({ position }) => {
-  return (
-    <Marker
-      position={position}
-      icon={{ url: "/defender.svg" }}
-      label={{ text: "You are here", color: "red" }}
-    />
-  );
+  return <Marker position={position} icon={{ url: "/defender.svg" }} />;
 };
