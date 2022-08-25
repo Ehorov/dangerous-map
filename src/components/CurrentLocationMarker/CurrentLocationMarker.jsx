319f6a91325c20ff85f5cb6897718de3b8920cc4
Ehorov <82938317+Ehorov@users.noreply.github.com>
2022-08-25 13:20:10 +0300

    next test

--- src/components/CurrentLocationMarker/CurrentLocationMarker.jsx
+++ src/components/CurrentLocationMarker/CurrentLocationMarker.jsx
@@ -1,5 +1,11 @@
 import { Marker } from '@react-google-maps/api';
 
 export const CurrentLocationMarker = ({ position }) => {
-  return <Marker position={position} icon={{ url: '/public/defender.svg' }} />;
+  return <Marker position={position} icon={{ url: '/defender.svg' }} />;
 };
+
+// export const Marker = ({ position }) => {
+//   return (
+//     <GoogleMapMarker position={position} icon={{ url: '/location.svg' }} />
+//   );
+// };
